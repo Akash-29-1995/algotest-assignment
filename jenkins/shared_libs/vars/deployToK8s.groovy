297b90121c43931c4e074c0ec0cd2adb2ac30835
Akash-29-1995 <akashdeepyadav0@gmail.com>
2025-06-13 02:48:19 +0530

    changing16

--- jenkins/shared_libs/vars/deployToK8s.groovy
+++ jenkins/shared_libs/vars/deployToK8s.groovy
@@ -40,7 +40,7 @@
 //               --docker-password=$DO_PASS
       
 //             echo "🔁 Replacing image tag with current build number..."
-//             sed -i 's|registry.digitalocean.com/algotest-registry/flask-app:latest|registry.digitalocean.com/algotest-registry/flask-app:${BUILD_NUMBER}|g' algotest-assignment/flask-app/k8s/deployment.yaml
+//             sed -i 's|registry.digitalocean.com/mytest-registry/flask-app:latest|registry.digitalocean.com/mytest-registry/flask-app:${BUILD_NUMBER}|g' algotest-assignment/flask-app/k8s/deployment.yaml
 //             echo "✅ Deploying Kubernetes manifests..."
 //             kubectl apply -f algotest-assignment/flask-app/k8s/deployment.yaml
 //             kubectl apply -f algotest-assignment/flask-app/k8s/service.yaml
@@ -78,7 +78,7 @@ def call() {
               --docker-password=$DO_PASS
       
             echo "🔁 Replacing image tag with current build number..."
-            sed -i 's|registry.digitalocean.com/algotest-registry/flask-app:latest|registry.digitalocean.com/algotest-registry/flask-app:${BUILD_NUMBER}|g' algotest-assignment/flask-app/k8s/deployment.yaml
+            sed -i 's|registry.digitalocean.com/mytest-registry/flask-app:latest|registry.digitalocean.com/mytest-registry/flask-app:${BUILD_NUMBER}|g' algotest-assignment/flask-app/k8s/deployment.yaml
 
             echo "✅ Deploying Kubernetes manifests..."
             kubectl apply -f algotest-assignment/flask-app/k8s/deployment.yaml
