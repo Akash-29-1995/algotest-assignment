6d3187a4f1e4606c40da888cec5652aa8915008b
Akash-29-1995 <akashdeepyadav0@gmail.com>
2025-06-12 22:49:32 +0530

    dhjkvg

--- jenkins/shared_libs/vars/deployToK8s.groovy
+++ jenkins/shared_libs/vars/deployToK8s.groovy
@@ -1,8 +1,21 @@
+// def call() {
+//     withKubeConfig([credentialsId: 'do-kubeconfig']) {
+//         sh '''
+//             echo "PWD: $(pwd)"
+//             ls -R
+//             kubectl apply -f algotest-assignment/flask-app/k8s/deployment.yaml
+//             kubectl apply -f algotest-assignment/flask-app/k8s/service.yaml
+//             kubectl apply -f algotest-assignment/flask-app/k8s/ingress.yaml
+//         '''
+//     }
+// }
+
 def call() {
-    withKubeConfig([credentialsId: 'do-kubeconfig']) {
+    withCredentials([string(credentialsId: 'do-access-token', variable: 'DO_ACCESS_TOKEN')]) {
         sh '''
-            echo "PWD: $(pwd)"
-            ls -R
+            mkdir -p ~/.kube
+            doctl auth init -t $DO_ACCESS_TOKEN
+            doctl kubernetes cluster kubeconfig save algotest-k8s
             kubectl apply -f algotest-assignment/flask-app/k8s/deployment.yaml
             kubectl apply -f algotest-assignment/flask-app/k8s/service.yaml
             kubectl apply -f algotest-assignment/flask-app/k8s/ingress.yaml
