fef413279b3626daed9fce946bfaf54dbe81c407
Akash-29-1995 <akashdeepyadav0@gmail.com>
2025-06-13 02:28:10 +0530

    changing14

--- jenkins/shared_libs/vars/deployToK8s.groovy
+++ jenkins/shared_libs/vars/deployToK8s.groovy
@@ -13,44 +13,39 @@
 
 
 // def call() {
-//     withCredentials([string(credentialsId: 'do-access-token', variable: 'DO_ACCESS_TOKEN')]) {
-//         withEnv(["KUBECONFIG=/tmp/kube/config"]) {
-//             sh '''
-//                 echo "✅ Installing doctl..."
-//                 curl -sL https://github.com/digitalocean/doctl/releases/download/v1.108.0/doctl-1.108.0-linux-amd64.tar.gz -o doctl.tar.gz
-//                 tar -xzf doctl.tar.gz
-//                 mv doctl /usr/local/bin/
-//                 chmod +x /usr/local/bin/doctl
+//     withCredentials([
+//         string(credentialsId: 'do-access-token', variable: 'DO_ACCESS_TOKEN'),
+//         usernamePassword(credentialsId: 'docr-creds', usernameVariable: 'DO_USER', passwordVariable: 'DO_PASS')
+//     ]) {
+//         sh '''
+//             echo "✅ Installing doctl..."
+//             curl -sL https://github.com/digitalocean/doctl/releases/download/v1.108.0/doctl-1.108.0-linux-amd64.tar.gz -o doctl.tar.gz
+//             tar -xzf doctl.tar.gz
+//             mv doctl /usr/local/bin/
+//             chmod +x /usr/local/bin/doctl
 
-//                 echo "$DO_ACCESS_TOKEN" | doctl auth init -t $(cat)
+//             echo "✅ Setting up kubeconfig..."
+//             mkdir -p /tmp/kube
+//             export KUBECONFIG=/tmp/kube/config
+//             echo "export KUBECONFIG=/tmp/kube/config" >> ~/.bashrc
 
-//                 echo "✅ Setting up kubeconfig..."
-//                 mkdir -p /tmp/kube
-//                 doctl kubernetes cluster kubeconfig show algotest-k8s > /tmp/kube/config
+//             doctl auth init -t $DO_ACCESS_TOKEN
+//             doctl kubernetes cluster kubeconfig save algotest-k8s --set-current-context=false
 
-//                 echo "✅ Verifying access..."
-//                 kubectl get nodes
-
-//                 echo "✅ Deploying Kubernetes manifests..."
-//                 kubectl apply -f algotest-assignment/flask-app/k8s/deployment.yaml
-//                 kubectl apply -f algotest-assignment/flask-app/k8s/service.yaml
-//                 kubectl apply -f algotest-assignment/flask-app/k8s/ingress.yaml
-//             '''
-
-//             timeout(time: 2, unit: 'MINUTES') {
-//                 waitUntil {
-//                     script {
-//                         def ip = sh(
-//                             script: "kubectl get svc flask-service -o jsonpath='{.status.loadBalancer.ingress[0].ip}'",
-//                             returnStdout: true
-//                         ).trim()
-//                         return ip && ip != "''"
-//                     }
-//                 }
-//             }
-
-//             echo "✅ LoadBalancer IP is ready."
-//         }
+//             echo "✅ Creating imagePullSecret in Kubernetes..."
+//             kubectl delete secret do-docker-creds --ignore-not-found=true
+//             kubectl create secret docker-registry do-docker-creds \
+//               --docker-server=registry.digitalocean.com \
+//               --docker-username=$DO_USER \
+//               --docker-password=$DO_PASS
+      
+//             echo "🔁 Replacing image tag with current build number..."
+//             sed -i 's|registry.digitalocean.com/algotest-registry/flask-app:latest|registry.digitalocean.com/algotest-registry/flask-app:${BUILD_NUMBER}|g' algotest-assignment/flask-app/k8s/deployment.yaml
+//             echo "✅ Deploying Kubernetes manifests..."
+//             kubectl apply -f algotest-assignment/flask-app/k8s/deployment.yaml
+//             kubectl apply -f algotest-assignment/flask-app/k8s/service.yaml
+//             kubectl apply -f algotest-assignment/flask-app/k8s/ingress.yaml
+//         '''
 //     }
 // }
 
@@ -60,7 +55,7 @@ def call() {
         string(credentialsId: 'do-access-token', variable: 'DO_ACCESS_TOKEN'),
         usernamePassword(credentialsId: 'docr-creds', usernameVariable: 'DO_USER', passwordVariable: 'DO_PASS')
     ]) {
-        sh '''
+        sh """
             echo "✅ Installing doctl..."
             curl -sL https://github.com/digitalocean/doctl/releases/download/v1.108.0/doctl-1.108.0-linux-amd64.tar.gz -o doctl.tar.gz
             tar -xzf doctl.tar.gz
@@ -84,10 +79,11 @@ def call() {
       
             echo "🔁 Replacing image tag with current build number..."
             sed -i 's|registry.digitalocean.com/algotest-registry/flask-app:latest|registry.digitalocean.com/algotest-registry/flask-app:${BUILD_NUMBER}|g' algotest-assignment/flask-app/k8s/deployment.yaml
+
             echo "✅ Deploying Kubernetes manifests..."
             kubectl apply -f algotest-assignment/flask-app/k8s/deployment.yaml
             kubectl apply -f algotest-assignment/flask-app/k8s/service.yaml
             kubectl apply -f algotest-assignment/flask-app/k8s/ingress.yaml
-        '''
+        """
     }
 }
