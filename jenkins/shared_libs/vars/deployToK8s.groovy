fb2e3c56fdb0cb6ef3d6fafc84312db66cca724b
Akash-29-1995 <akashdeepyadav0@gmail.com>
2025-06-12 23:00:36 +0530

    dhjkvsgedwqwdq

--- jenkins/shared_libs/vars/deployToK8s.groovy
+++ jenkins/shared_libs/vars/deployToK8s.groovy
@@ -10,22 +10,44 @@
 //     }
 // }
 
+// def call() {
+//     withCredentials([string(credentialsId: 'do-access-token', variable: 'DO_ACCESS_TOKEN')]) {
+//         sh '''
+//             echo "PWD: $(pwd)"
+//             ls -R
+
+//             echo "✅ Setting up kubeconfig using doctl..."
+//             mkdir -p ~/.kube
+
+//             doctl auth init -t $DO_ACCESS_TOKEN
+//             doctl kubernetes cluster kubeconfig save algotest-k8s
+
+//             echo "✅ Applying Kubernetes manifests..."
+//             kubectl apply -f algotest-assignment/flask-app/k8s/deployment.yaml
+//             kubectl apply -f algotest-assignment/flask-app/k8s/service.yaml
+//             kubectl apply -f algotest-assignment/flask-app/k8s/ingress.yaml
+//         '''
+//     }
+// }
+
 def call() {
     withCredentials([string(credentialsId: 'do-access-token', variable: 'DO_ACCESS_TOKEN')]) {
-        sh '''
-            echo "PWD: $(pwd)"
+        sh """
+            echo "PWD: \$(pwd)"
             ls -R
-            
+
             echo "✅ Setting up kubeconfig using doctl..."
             mkdir -p ~/.kube
 
-            doctl auth init -t $DO_ACCESS_TOKEN
+            doctl auth init -t \$DO_ACCESS_TOKEN
             doctl kubernetes cluster kubeconfig save algotest-k8s
 
             echo "✅ Applying Kubernetes manifests..."
             kubectl apply -f algotest-assignment/flask-app/k8s/deployment.yaml
             kubectl apply -f algotest-assignment/flask-app/k8s/service.yaml
             kubectl apply -f algotest-assignment/flask-app/k8s/ingress.yaml
-        '''
+        """
     }
 }
+
+
