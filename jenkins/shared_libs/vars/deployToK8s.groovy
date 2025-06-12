031e0844e0adafd82f05478bab163c3b443b993a
Akash-29-1995 <akashdeepyadav0@gmail.com>
2025-06-12 22:35:33 +0530

    making changes

--- jenkins/shared_libs/vars/deployToK8s.groovy
+++ jenkins/shared_libs/vars/deployToK8s.groovy
@@ -1,6 +1,8 @@
 def call() {
     withKubeConfig([credentialsId: 'do-kubeconfig']) {
         sh """
+            echo "PWD: $(pwd)"
+            ls -R
             kubectl apply -f flask-app/k8s/deployment.yaml
             kubectl apply -f flask-app/k8s/service.yaml
             kubectl apply -f flask-app/k8s/ingress.yaml
