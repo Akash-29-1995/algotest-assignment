940ff947ec31a04a37b38d046387735e0635a0fb
Akash-29-1995 <akashdeepyadav0@gmail.com>
2025-06-12 22:50:34 +0530

    dhjkvsg

--- jenkins/shared_libs/vars/deployToK8s.groovy
+++ jenkins/shared_libs/vars/deployToK8s.groovy
@@ -13,9 +13,13 @@
 def call() {
     withCredentials([string(credentialsId: 'do-access-token', variable: 'DO_ACCESS_TOKEN')]) {
         sh '''
+            echo "✅ Setting up kubeconfig using doctl..."
             mkdir -p ~/.kube
+
             doctl auth init -t $DO_ACCESS_TOKEN
             doctl kubernetes cluster kubeconfig save algotest-k8s
+
+            echo "✅ Applying Kubernetes manifests..."
             kubectl apply -f algotest-assignment/flask-app/k8s/deployment.yaml
             kubectl apply -f algotest-assignment/flask-app/k8s/service.yaml
             kubectl apply -f algotest-assignment/flask-app/k8s/ingress.yaml
